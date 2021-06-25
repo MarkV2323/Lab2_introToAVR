e4d7a038c9bd87db6ce6f3459b3d6cfdc8533277
Mark Vincent <41304553+MarkV2323@users.noreply.github.com>
2021-06-24 21:44:24 -0700

    Finished part 1 of lab

--- source/main.c
+++ source/main.c
@@ -1,8 +1,8 @@
-/*	Author: lab
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
+/*	Author: Mark Vincent II
+ *  Partner(s) Name: NA
+ *	Lab Section: A01
+ *	Assignment: Lab #2  Exercise #1
+ *	Exercise Description: In this exercise we need to setup a garage door system based on the truth table provided in the lab manual.
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -14,11 +14,39 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-    DDRB = 0xFF; // Configure port B's 8 pins as outputs
+    DDRA = 0x00; // Configure port A's 8 pins as inputs
+	PORTA = 0xFF;
+    
+	DDRB = 0xFF; // Configure port B's 8 pins as outputs
     PORTB = 0x00; // Initialize PORTB output to 0’s    
-    /* Insert your solution below */
+
+	unsigned char tmpA = 0x00; // Temporary varible to hold value of A
+	unsigned char tmpB = 0x00; // Temporary varible to hold value of B
+
+	/* Insert your solution below */
     while (1) {
-        PORTB = 0x0F; // Writes port B's 8 pins with 00001111
-    }
-    return 1;
+    	tmpA = PINA & 0x03; // Masks input pins, A1 A0 into tmpA.
+
+		// Begins checking tmpA to compute logic
+		if (tmpA == 0x00) {
+			// B0 should be 0
+			tmpB = (tmpB & 0xFE) | 0x00; // does a mask of B besides B0, than sets B0 to 0. bbbb bbb0
+		} else if (tmpA == 0x01) {
+			// B0 should be 1
+			tmpB = (tmpB & 0xFE) | 0x01; // does a mask of B besides B0, than sets B0 to 1. bbbb bbb1
+		} else if (tmpA == 0x2) {
+			// B0 should be 0
+			tmpB = (tmpB & 0xFE) | 0x00; // does a mask of B besides B0, than sets B0 to 0. bbbb bbb0
+		} else if (tmpA == 0x3) {
+			// B0 should be 0
+			tmpB = (tmpB & 0xFE) | 0x00; // does a mask of B besides B0, than sets B0 to 0. bbbb bbb0
+		} else {
+			// Some other input value not regonized, ignored.
+		}
+
+		// writes B
+		PORTB = tmpB;
+	}
+ 
+	return 0;
 }
